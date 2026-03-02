11dd0f4c55e17f097bd0fbf127890f11792ca30a
rikarahmawati88 <rikarahmawati_2327240095@mhs.mdp.ac.id>
2026-03-02 15:23:42 +0700

    revv

---
+++ karyawan/lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
