a854ab0327285f067db4209f253ca8b3e3074857
rikarahmawati88 <rikarahmawati_2327240095@mhs.mdp.ac.id>
2026-04-06 17:11:34 +0700

    update

--- mdp_gold/lib/screens/price_list_screen.dart
+++ mdp_gold/lib/screens/price_list_screen.dart
@@ -1,8 +1,7 @@
-import "package:flutter/widgets.dart";
+import 'package:flutter/widgets.dart';
 import "package:flutter/material.dart";
 import "package:firebase_database/firebase_database.dart";
 import "package:mdp_gold/services/gold_service.dart";
-
 
 class PriceListScreen extends StatefulWidget {
   const PriceListScreen({super.key});
@@ -18,41 +17,45 @@ class _PriceListScreenState extends State<PriceListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Harga Emas'),
+        title: Text('Harga Emas'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
       body: StreamBuilder<DatabaseEvent>(
         stream: _goldService.getPriceList(),
         builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-                   return const Center(child: CircularProgressIndicator());
-                  }
-        if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  }
-        final data = snapshot.data?.snapshot.value;  
-        final Map<dynamic, dynamic> itemsMap =
-                        data as Map<dynamic, dynamic>;
-                    // Mengubah Map menjadi List agar bisa diiterasi
-        final items = itemsMap.entries.toList();
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        return ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = Map<String, dynamic>.from(
-            items[index].value as Map,
-          );
-          final String tanggal = item['tanggal'] ?.toString()?? '';
-          final String harga = item['harga'] ?.toString()?? '';
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
 
-          return ListTile(
-            title: Text(harga),
-            subtitle: Text(tanggal),
+          final data = snapshot.data?.snapshot.value;
+
+          final Map<dynamic, dynamic> itemsMap = data as Map<dynamic, dynamic>;
+
+          final items = itemsMap.entries.toList();
+
+          return ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = Map<String, dynamic>.from(
+                          items[index].value as Map,
+                        );
+
+              final String tanggal = item['tanggal']?.toString() ?? '';
+              final String harga = item['harga']?.toString() ?? '';
+
+              return ListTile(
+                title: Text(harga),
+                subtitle: Text(tanggal),
+              );
+            },
           );
-        },
-      );
-    ),
-  };
+        }, 
+      ),
+    );
+  }
 }
-
